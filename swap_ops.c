0fb72b5db2f9af7a24837ebd384af0886ec32084
olusiekwin <olusiekwingraham@gmail.com>
2023-08-25 02:10:25 +0300

    betty-Fixes

--- swap_ops.c
+++ swap_ops.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+* swap - Swaps the top two elements of the stack
+* @stack: Pointer to the top of the stack
+* @line_number: Line number in the Monty bytecode file
+*/
 void swap(stack_t **stack, unsigned int line_number)
 {
 stack_t *top, *swap;
